799011f9759b16640fdf515102ce43935f78751d
Akin Onisile <69411318+simple31282@users.noreply.github.com>
2022-07-14 17:43:22 -0400

    Add files via upload

---
+++ src/test/java/org/generation/itemsApi/ItemsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.itemsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
